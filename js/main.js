a7a875197d8dc6af4f0137377b37a90e07db6a14
Your Name <you@example.com>
2022-07-17 14:35:15 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 1500
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+});
